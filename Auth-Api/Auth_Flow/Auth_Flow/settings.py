a37d2221890069e4d7cbea6e3dd93d1067de9ba6
Tomide Adeyanju <tadeyanju@infinion.co>
2025-12-02 12:08:32 +0100

    added allowed host

--- Auth-Api/Auth_Flow/Auth_Flow/settings.py
+++ Auth-Api/Auth_Flow/Auth_Flow/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-3cw^&=%=%ao$!-hqs(c2#acj+nxoj*#8w6wt)pum1u_or^uo$s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['authlogin-csefayf7ergtb0ez.canadacentral-01.azurewebsites.net', "127.0.0.1", "auth-api-1-5lwx.onrender.com"]
+ALLOWED_HOSTS = ['authlogin-csefayf7ergtb0ez.canadacentral-01.azurewebsites.net', "127.0.0.1", "auth-api-1-5lwx.onrender.com", "auth-api-2-j49c.onrender.com"]
 
 
 # Application definition
